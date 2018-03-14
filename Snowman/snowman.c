e21c2f3b8643362efc07e20b3fb7815f3d8c77d2
Lucas Litter Mentz <lucaslittermentz@gmail.com>
2018-03-13 21:53:51 -0300

    Clarear boneco e fundo

--- Snowman/snowman.c
+++ Snowman/snowman.c
@@ -1,11 +1,13 @@
-//gcc snowman.c -lglut -lGL -lGLU -o snowman && ./snowman
-
+//gcc snowman.c -lglut -lGL -lGLU -lm -o snowman && ./snowman
 #include <GL/freeglut.h>
 #include <GL/gl.h>
 #include <GL/glut.h>
+#include <math.h>
 
 void drawScene(void);
 float angle = 0;
+float bob = 0;
+unsigned int count;
 
 int main(int argc, char** argv) {
     glutInit(&argc, argv);
@@ -16,7 +18,7 @@ int main(int argc, char** argv) {
     glEnable(GL_DEPTH_TEST);
     glutDisplayFunc(drawScene);
     glutIdleFunc(drawScene);
-    //glDepthFunc(GL_LEQUAL);
+    glDepthFunc(GL_LEQUAL);
 
     glutMainLoop();
 
@@ -26,38 +28,40 @@ int main(int argc, char** argv) {
 void drawScene(void) {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     glLoadIdentity();
-    //glClearColor(0.3, 0.3, 0.3, 0.3);
+    glClearColor(0.25, 0.25, 0.25, 0.3);
     angle += 1;
-    //if(angle >= 360.0)
-    //    angle = 0.0;
+    if(angle >= 360)
+        angle = 0;
+    bob = sin(count++ / (M_PI * 12)) / 16.0;
 
     glRotated(angle, 0, 1, 0);
+    glTranslated(0.0, bob, 0.0);
 
     glPushMatrix();
         //Corpo e cabeça
-        glColor4f(0.5, 0.5, 0.5, 1);
-        glTranslated(0.0, -0.3, 0);
-        glutSolidSphere(0.45, 10, 10);
+        glColor4f(0.75, 0.75, 0.78, 1);
+        glTranslated(0.0, -0.25, 0);
+        glutSolidSphere(0.45, 12, 12);
         glTranslated(0.0, 0.4, 0);
-        glutSolidSphere(0.30, 10, 10);
+        glutSolidSphere(0.30, 12, 12);
         glTranslated(0.0, 0.35, 0);
-        glutSolidSphere(0.25, 10, 10);
+        glutSolidSphere(0.25, 12, 12);
 
         //Detalhes da cabeça
         //Nariz
         glColor4f(0.8, 0.5, 0, 1);
         glTranslated(0.0, 0.0, 0.235);
-        glutSolidCone(0.065, 0.25, 10, 10);
+        glutSolidCone(0.065, 0.25, 12, 12);
 
         glColor4f(101/255.0, 69/255.0, 26/255.0, 1); // Cor marrom
 
         //Olho esquerdo
         glTranslated(-0.15, 0.065, -0.075);
-        glutSolidSphere(0.055, 10, 10);
+        glutSolidSphere(0.055, 12, 12);
 
         //Olho direito
         glTranslated(0.30, 0, 0);
-        glutSolidSphere(0.055, 10, 10);
+        glutSolidSphere(0.055, 12, 12);
     glPopMatrix();
 
     glFlush();
